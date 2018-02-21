642d6125deb1fe2b3e2523ae2fdf66b55b7cee3f
Rossi Oddet <rossi.oddet@dev-institut.fr>
2018-02-21 21:52:17 +0100

    test clever deploy

---
+++ src/main/java/dev/controller/VersionController.java
@@ -0,0 +1,30 @@
+package dev.controller;
+
+import dev.domain.Version;
+import dev.repository.VersionRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class VersionController {
+
+    @Value("${app.version}")
+    private String appVersion;
+    private VersionRepository versionRepo;
+
+    @EventListener(ContextRefreshedEvent.class)
+    public void onStart() {
+        this.versionRepo.save(new Version(appVersion));
+    }
+
+    @GetMapping("/version")
+    public List<Version> getVersion() {
+        return this.versionRepo.findAll();
+    }
+
+}
